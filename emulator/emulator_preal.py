ab015ab0dd04323584a462e2664c4da3802d23c0
Martin White <martinjameswhite@users.noreply.github.com>
2022-01-26 20:27:20 -0800

    Pass file name for json file.
    Modify the init and load methods to read the path of the JSON file for the emulator.

--- emulator/emulator_preal.py
+++ emulator/emulator_preal.py
@@ -5,34 +5,30 @@ from taylor_approximation import taylor_approximate
 
 class Emulator_Preal(object):
     
-    def __init__(self, filebase):
+    def __init__(self, emufilename):
 
-        self.load(filebase)
+        self.load(emufilename)
         
         self.cpars = np.zeros(3)
         self.ptabs  = {}
-        
-    
-    def load(self, filebase):
-        
-        # Load Taylor Series json file
-        json_file = open('emu/preal.json', 'r')
+        #
+    def load(self, emufilename):
+        '''Load the Taylor series from emufilename and repackage it into
+           a dictionary of arrays.'''
+        # Load Taylor series json file from emufilename:
+        json_file = open(emufilename,'r')
         emu = json.load( json_file )
         json_file.close()
         
-        # repackage into form we need, i.e. arrays
-        
+        # repackage into form we need, i.e. a dictionary of arrays
         self.emu_dict = {}
-        
         for zstr in emu.keys():
-            
             self.emu_dict[zstr] = {'kvec': np.array(emu[zstr]['kvec']),\
                                    'x0': emu[zstr]['x0'],\
                                    'derivs': [np.array(ll) for ll in emu[zstr]['derivs']]}
-        
-        del emu
-        
-    
+        # and just do some tidying up.
+        del(emu)
+        #
     def update_cosmo(self, cpars):
         '''
         If the cosmology is not the same as the old one, update the ptables.
@@ -43,17 +39,16 @@ class Emulator_Preal(object):
                 self.ptabs[zstr] = taylor_approximate(cpars,\
                                                       self.emu_dict[zstr]['x0'],\
                                                       self.emu_dict[zstr]['derivs'], order=4)
-                
-        
-    
+        #
     def __call__(self, params, spectra='Pgg'):
-        
-        zstr = str(params[-1])
+        '''Evaluate the Taylor series for the spectrum given by 'spectra'
+           at the point given by 'params'.'''
+        zstr  = str(params[-1])
         cpars = params[:3]
         bpars = params[3:-1]
-        
+        #
         self.update_cosmo(cpars)
-        
+        #
         if spectra == 'Pgg':
             b1, b2, bs,b3, alpha, sn,= bpars
             bias_monomials = np.array([1, b1, b1**2,\
@@ -67,20 +62,15 @@ class Emulator_Preal(object):
                                0.5*b2, 0, 0,\
                                0.5*bs, 0, 0, 0,\
                                0.5*b3, 0])
-        
         elif spectra =='Pmm':
-            alpha = bpars
-            sn = 0
+            alpha,sn = bpars,0
             bias_monomials = np.array([1, 0, 0,\
                                0, 0, 0,\
                                0, 0, 0, 0,\
                                0, 0])
-            
         kvec = self.ptabs[zstr][:,0]
         za   = self.ptabs[zstr][:,-1]
-    
         # the first row is kv, last row is za for countrterm
         res = np.sum(self.ptabs[zstr][:,1:-1] * bias_monomials,axis=1)\
               + alpha * kvec**2 * za + sn
-    
-        return kvec, res
+        return kvec,res
