a7d8974622b0c688ac76e3d195a5211d7bb6a1b8
Martin White <mwhite@berkeley.edu>
2022-01-26 20:56:51 -0800

    First draft.

---
+++ gxk_likelihood/gxk_likelihood.py
@@ -0,0 +1,155 @@
+import numpy as np
+import sys
+import os
+
+from scipy.interpolate import InterpolatedUnivariateSpline as Spline
+from emulator_preal    import Emulator_Preal               as Emulator
+from predict_cl        import AngularPowerSpectra
+from cobaya.likelihood import Likelihood
+
+
+
+
+class GxKLikelihood(Likelihood):
+    # From yaml file.
+    model:  str
+    clsfn:  str
+    covfn:  str
+    Emufn:  str
+    zeff:   list
+    suffx:  list
+    dndzfn: list
+    wlafn:  list
+    wlxfn:  list
+    acut:   list
+    xcut:   list
+    #
+    def initialize(self):
+        """Sets up the class."""
+        # Load the data and invert the covariance matrix.
+        self.loadData()
+        self.cinv   = np.linalg.inv(self.cov)
+        self.Emu    = Emulator(self.Emufn)
+    def get_requirements(self):
+        """What we require."""
+        zgrid= np.logspace(0,3.1,64) - 1.0
+        reqs = {\
+               'logA':     None,\
+               'omch2':    None,\
+               'ombh2':    None,\
+               'H0':       None,\
+               'ns':       None,\
+               'omegam':   None,\
+               'sigma8_z': {'z': [0]},\
+               'Hubble':   {'z': zgrid},\
+               'comoving_radial_distance': {'z': zgrid}\
+               }
+        # Build the parameter names we require for each sample.
+        for suf in self.suffx:
+            for pref in ['b1','b2','bs','bn',\
+                         'alpha_a','alpha_x','alpha_m',\
+                         'SN','smag']:
+                reqs[pref+'_'+suf] = None
+        return(reqs)
+    def logp(self,**params_values):
+        """Return the log-likelihood."""
+        pp  = self.provider
+        OmM = pp.get_param('omegam')
+        sig8= pp.get_sigma8_z(0)[0]
+        hub = pp.get_param('H0')/100.0
+        # Make splines for chi(z) and E(z), converting to Mpc/h.
+        zgrid = np.logspace(0,3.1,64)-1.0
+        chiz  = pp.get_comoving_radial_distance(zgrid)*hub
+        chiz  = Spline(zgrid,chiz)
+        Eofz  = pp.get_Hubble(zgrid)
+        Eofz  = Spline(zgrid,Eofz/(100*hub))
+        #
+        obs = np.array([],dtype='float')
+        for i,suf in enumerate(self.suffx):
+            zeff= float(self.zeff[i])
+            aps = AngularPowerSpectra(OmM,chiz,Eofz,self.dndz[i],zeff)
+            # Fill in the parameter list, starting with the
+            # cosmological parameters.
+            if self.model.startswith('clpt'):
+                cpars = [OmM,hub,sig8]
+            else:
+                raise RuntimeError("Unknown model.")
+            # Extract some common parameters.
+            b1  = pp.get_param('b1_'+suf)
+            b2  = pp.get_param('b2_'+suf)
+            bs  = pp.get_param('bs_'+suf)
+            sn  = pp.get_param('SN_'+suf)
+            smag= pp.get_param('smag_'+suf)
+            #
+            # Do some parameter munging depending upon the model name
+            # to fill in the rest of pars.
+            if self.model.startswith('clpt'):
+                alpA  = pp.get_param('alpha_a_'+suf)
+                alpX  = pp.get_param('alpha_x_'+suf)
+                alpM  = pp.get_param('alpha_m_'+suf)
+                bparsA= [b1,b2,bs,alpA,sn]
+                bparsX= [b1,b2,bs,alpX]
+                bparsM= [alpM]
+            else:
+                raise RuntimeError("Unknown model.")
+            # and call APS to get a prediction,
+            ell,clgg,clgk = aps(self.Emu,\
+                                cpars,bparsA,bparsX,bparsM,\
+                                smag,Lmax=1251)
+            thy = np.array([ell,clgg,clgk]).T
+            # then "observe" it, appending the observations to obs.
+            obs = np.append(obs,self.observe(thy,self.wla[i],self.wlx[i]))
+        # Now compute chi^2 and return -ln(L)
+        chi2 = np.dot(self.dd-obs,np.dot(self.cinv,self.dd-obs))
+        return(-0.5*chi2)
+        #
+    def loadData(self):
+        """Load the data, covariance and windows from files."""
+        dd        = np.loadtxt(self.clsfn)
+        self.cov  = np.loadtxt(self.covfn)
+        self.dndz = []
+        for fn in self.dndzfn:
+            self.dndz.append(np.loadtxt(fn))
+        self.wla = []
+        for fn in self.wlafn:
+            self.wla.append(np.loadtxt(fn))
+        self.wlx = []
+        for fn in self.wlxfn:
+            self.wlx.append(np.loadtxt(fn))
+        # Now pack things and modify the covariance matrix to
+        # "drop" some data points.
+        Nsamp   = (dd.shape[1]-1)//2
+        if Nsamp!=len(self.dndz):
+            raise RuntimeError("Inconsistent inputs: Nsamp mismatch.")
+        if Nsamp!=len(self.wla):
+            raise RuntimeError("Inconsistent inputs: Nsamp mismatch.")
+        if Nsamp!=len(self.wlx):
+            raise RuntimeError("Inconsistent inputs: Nsamp mismatch.")
+        if Nsamp!=len(self.acut):
+            raise RuntimeError("Inconsistent inputs: Nsamp mismatch.")
+        if Nsamp!=len(self.xcut):
+            raise RuntimeError("Inconsistent inputs: Nsamp mismatch.")
+        self.xx = dd[:,0]
+        self.dd = dd[:,1:].T.flatten()
+        for j in range(Nsamp):
+            for i in np.nonzero(self.xx>self.acut[j])[0]:           # Auto
+                ii = i + (2*j+0)*self.xx.size
+                self.cov[ii, :] = 0
+                self.cov[ :,ii] = 0
+                self.cov[ii,ii] = 1e15
+            for i in np.nonzero(self.xx>self.xcut[j])[0]:           # Cross
+                ii = i + (2*j+1)*self.xx.size
+                self.cov[ii, :] = 0
+                self.cov[ :,ii] = 0
+                self.cov[ii,ii] = 1e15
+        #
+    def observe(self,tt,wla,wlx):
+        """Applies the window function and binning matrices."""
+        lmax = wla.shape[1]
+        ells = np.arange(lmax)
+        # Have to stack auto and cross.
+        obs1 = np.dot(wla,np.interp(ells,tt[:,0],tt[:,1],right=0))
+        obs2 = np.dot(wlx,np.interp(ells,tt[:,0],tt[:,2],right=0))
+        obs  = np.concatenate([obs1,obs2])
+        return(obs)
+        #
---
+++ gxk_likelihood/predict_cl.py
@@ -0,0 +1,135 @@
+#!/usr/bin/env python3
+#
+# Code to compute angular power spectra using Limber's approximation,
+# ignoring higher-order corrections such as curved sky or redshift-space
+# distortions (that predominantly affect low ell).
+#
+import numpy as np
+import sys
+
+from scipy.integrate   import simps
+from scipy.interpolate import InterpolatedUnivariateSpline as Spline
+from scipy.special     import hyp2f1
+
+
+def D_of_z(OmM,zz):
+    """Scale-independent growth factor for flat LCDM."""
+    aa = 1./(1.+zz)
+    rr = (1-OmM)/OmM
+    t1 = hyp2f1(1./3,1,11./6,-aa**3*rr)
+    t2 = hyp2f1(1./3,1,11./6,-rr)
+    return( aa * t1/t2 )
+    #
+
+
+
+
+
+class AngularPowerSpectra():
+    """Computes angular power spectra using the Limber approximation."""
+    def lagrange_spam(self,z):
+        """Returns the weights to apply to each z-slice to interpolate to z.
+           -- Not currently used, could be used if need P(k,z)."""
+        dz = self.zlist[:,None] - self.zlist[None,:]
+        singular = (dz == 0)
+        dz[singular] = 1.0
+        fac = (z - self.zlist) / dz
+        fac[singular] = 1.0
+        return(fac.prod(axis=-1))
+        #
+    def mag_bias_kernel(self,s,Nchi_mag=101):
+        """Returns magnification bias kernel if 's' is the slope of
+           the number counts dlog10N/dm."""
+        zval    = self.zchi(self.chival)
+        cmax    = np.max(self.chival) * 1.1
+        zupper  = lambda x: np.linspace(x,cmax,Nchi_mag)
+        chivalp = np.array(list(map(zupper,self.chival))).transpose()
+        zvalp   = self.zchi(chivalp)
+        dndz_n  = np.interp(zvalp,self.zz,self.dndz,left=0,right=0)
+        Ez      = self.Eofz(zvalp)
+        g       = (chivalp-self.chival[np.newaxis,:])/chivalp
+        g      *= dndz_n*Ez/2997.925
+        g       = self.chival * simps(g,x=chivalp,axis=0)
+        mag_kern= 1.5*(self.OmM)/2997.925**2*(1+zval)*g*(5*s-2.)
+        return(mag_kern)
+        #
+    def shot3to2(self):
+        """Returns the conversion from 3D shotnoise to 2D shotnoise power."""
+        Cshot = self.fchi**2/self.chival**2
+        Cshot = simps(Cshot,x=self.chival)
+        return(Cshot)
+    def __init__(self,OmM,chi_of_z,E_of_z,dndz,zeff,Nchi=201,Nz=251):
+        """Set up the class.
+            OmM:  The value of Omega_m(z=0) for the cosmology.
+            chi_of_z: A function returning radial distance in Mpc/h given z.
+            E_of_z: A function returning H(z)/H(0) given z.
+            dndz: A numpy array (Nbin,2) containing dN/dz vs. z."""
+        # Copy the arguments, setting up the z-range.
+        self.Eofz = E_of_z
+        self.Nchi = Nchi
+        self.OmM  = OmM
+        self.OmX  = 1.0-OmM
+        self.zmin = np.min([0.05,dndz[0,0]])
+        self.zmax = dndz[-1,0]
+        self.zz   = np.linspace(self.zmin,self.zmax,Nz)
+        self.dndz = Spline(dndz[:,0],dndz[:,1],ext=1)(self.zz)
+        # Normalize dN/dz.
+        self.dndz = self.dndz/simps(self.dndz,x=self.zz)
+        # Set up the chi(z) array and z(chi) spline.
+        self.chiz = chi_of_z(self.zz)
+        self.zchi = Spline(self.chiz,self.zz)
+        # Work out W(chi) for the objects whose dNdz is supplied.
+        chimin    = np.min(self.chiz)
+        chimax    = np.max(self.chiz)
+        self.chival= np.linspace(chimin,chimax,Nchi)
+        zval      = self.zchi(self.chival)
+        self.fchi = Spline(self.zz,self.dndz*E_of_z(self.zz))(zval)
+        self.fchi/= simps(self.fchi,x=self.chival)
+        # and W(chi) for the CMB
+        self.chistar= chi_of_z(1098.)
+        self.fcmb = 1.5*self.OmM*(1.0/2997.925)**2*(1+zval)
+        self.fcmb*= self.chival*(self.chistar-self.chival)/self.chistar
+        # Set the effective redshift.
+        self.zeff = zeff
+        # and save linear growth.
+        self.ld2  = (D_of_z(self.OmM,zval)/D_of_z(self.OmM,self.zeff))**2
+        #
+    def __call__(self,Emu,cpars,bparsA,bparsX,bparsM,smag=0.4,\
+                 Nell=64,Lmax=1001):
+        """Computes C_l^{gg} and C_l^{kg} given the emulator for P_{ij}, the
+           cosmological parameters (cpars) plus bias params for auto (bparsA),
+           cross (bparsX) and matter (bparsM) and the magnification slope
+           (smag)."""
+        # Set up arrays to hold kernels for C_l.
+        ell    = np.logspace(1,np.log10(Lmax),Nell) # More ell's are cheap.
+        Cgg,Ckg= np.zeros( (Nell,self.Nchi) ),np.zeros( (Nell,self.Nchi) )
+        # The magnification bias kernel.
+        fmag   = self.mag_bias_kernel(smag)
+        # Fit splines to our P(k).  The spline extrapolates as needed.
+        pars   = np.array(cpars+bparsA+[self.zeff])
+        Pgg    = Spline(*Emu(pars,spectra='Pgg'))
+        pars   = np.array(cpars+bparsX+[self.zeff])
+        Pgm    = Spline(*Emu(pars,spectra='Pgm'))
+        pars   = np.array(cpars+bparsM+[self.zeff])
+        Pmm    = Spline(*Emu(pars,spectra='Pmm'),ext=1) # Extrap. w/ zeros.
+        # Work out the integrands for C_l^{gg} and C_l^{kg}.
+        for i,chi in enumerate(self.chival):
+            kval     = (ell+0.5)/chi        # The vector of k's.
+            f1f2     = self.fchi[i]*self.fchi[i]/chi**2 * Pgg(kval)
+            f1m2     = self.fchi[i]*     fmag[i]/chi**2 * Pgm(kval)
+            m1f2     =      fmag[i]*self.fchi[i]/chi**2 * Pgm(kval)
+            m1m2     =      fmag[i]*     fmag[i]/chi**2 * Pmm(kval)*self.ld2[i]
+            Cgg[:,i] = f1f2 + f1m2 + m1f2 + m1m2
+            f1f2     = self.fchi[i]*self.fcmb[i]/chi**2 * Pgm(kval)
+            m1f2     =      fmag[i]*self.fcmb[i]/chi**2 * Pmm(kval)*self.ld2[i]
+            Ckg[:,i] = f1f2 + m1f2
+        # and then just integrate them.
+        Cgg = simps(Cgg,x=self.chival,axis=-1)
+        Ckg = simps(Ckg,x=self.chival,axis=-1)
+        # Now interpolate onto a regular ell grid.
+        lval= np.arange(Lmax)
+        Cgg = Spline(ell,Cgg)(lval)
+        Ckg = Spline(ell,Ckg)(lval)
+        return( (lval,Cgg,Ckg) )
+        #
+
