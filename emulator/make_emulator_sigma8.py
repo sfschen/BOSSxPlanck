7ea7d00054f03d7e12d628054b3242f57a15c5d8
sfschen <shifan_chen@berkeley.edu>
2022-02-07 14:10:53 -0800

    sigma8 and xiells emulators.

---
+++ emulator/make_emulator_sigma8.py
@@ -0,0 +1,62 @@
+import numpy as np
+from mpi4py import MPI
+import sys
+
+from taylor_approximation import compute_derivatives
+from compute_class_sigma8 import compute_sigma8
+import json
+
+comm = MPI.COMM_WORLD
+mpi_rank = comm.Get_rank()
+mpi_size = comm.Get_size()
+print( "Hello I am process %d of %d." %(mpi_rank, mpi_size) )
+
+basedir = sys.argv[1]
+
+output_shape = (1,)
+
+# First construct the grid
+order = 4
+# these are OmegaM, h, sigma8
+x0s = [0.31, 0.68]; Nparams = len(x0s) # these are chosen to be roughly at the BOSS best fit value
+dxs = [0.01, 0.01]
+
+template = np.arange(-order,order+1,1)
+Npoints = 2*order + 1
+grid_axes = [ dx*template + x0 for x0, dx in zip(x0s,dxs)]
+
+Inds   = np.meshgrid( * (np.arange(Npoints),)*Nparams, indexing='ij')
+Inds = [ind.flatten() for ind in Inds]
+center_ii = (order,)*Nparams
+Coords = np.meshgrid( *grid_axes, indexing='ij')
+
+X0grid = np.zeros( (Npoints,)*Nparams + output_shape )
+X0gridii = np.zeros( (Npoints,)*Nparams + output_shape )
+
+for nn, iis in enumerate(zip(*Inds)):
+    if nn%mpi_size == mpi_rank:
+        coord = [Coords[i][iis] for i in range(Nparams)]
+        print(coord,iis)
+        X0gridii[iis] = compute_sigma8(coord)
+
+comm.Allreduce(X0gridii, X0grid, op=MPI.SUM)
+del(X0gridii)
+
+# Now compute the derivatives
+derivs0 = compute_derivatives(X0grid, dxs, center_ii, 5)
+
+# Now save:
+if mpi_rank == 0:
+
+    outfile = basedir+'emu/boss_s8.json'
+
+    list0 = [ dd.tolist() for dd in derivs0 ]
+
+    outdict = {'params': ['omegam', 'h'],\
+           'x0': x0s,\
+           'lnA0': 3.047,\
+           'derivs0': list0,}
+
+    json_file = open(outfile, 'w')
+    json.dump(outdict, json_file)
+    json_file.close()
---
+++ emulator/make_emulator_xiells.py
@@ -0,0 +1,89 @@
+import numpy as np
+import sys
+from mpi4py import MPI
+import json
+
+from compute_fid_dists import compute_fid_dists
+from compute_xiell_tables import compute_xiell_tables
+from taylor_approximation import compute_derivatives
+
+
+comm = MPI.COMM_WORLD
+mpi_rank = comm.Get_rank()
+mpi_size = comm.Get_size()
+print( "Hello I am process %d of %d." %(mpi_rank, mpi_size) )
+
+basedir = sys.argv[1]
+z = float(sys.argv[2])
+Omfid = float(sys.argv[3])
+
+# Set r grid:
+rmin, rmax, dr = 50, 160, 0.5
+
+# compute fiducial distances:
+fid_dists = compute_fid_dists(z,Omfid)
+
+# Remake the data grid:
+order = 4
+Npoints = 2*order + 1
+# these are OmegaM, h, sigma8
+x0s = [0.31, 0.68, 0.73]; Nparams = len(x0s)
+dxs = [0.01, 0.01, 0.05]
+
+# Set output shape
+rr = np.arange(rmin, rmax, dr)
+output_shape = (len(rr),6) # this is for 1, B1, F, B1*F, B1^2, F^2
+
+# Make parameter grid:
+template = np.arange(-order,order+1,1)
+Npoints = 2*order + 1
+grid_axes = [ dx*template + x0 for x0, dx in zip(x0s,dxs)]
+
+Inds   = np.meshgrid( * (np.arange(Npoints),)*Nparams, indexing='ij')
+Inds = [ind.flatten() for ind in Inds]
+center_ii = (order,)*Nparams
+Coords = np.meshgrid( *grid_axes, indexing='ij')
+
+# Compute the grid!
+X0grid = np.zeros( (Npoints,)*Nparams+ output_shape)
+X2grid = np.zeros( (Npoints,)*Nparams+ output_shape)
+
+X0gridii = np.zeros( (Npoints,)*Nparams+ output_shape)
+X2gridii = np.zeros( (Npoints,)*Nparams+ output_shape)
+
+for nn, iis in enumerate(zip(*Inds)):
+    if nn%mpi_size == mpi_rank:
+        coord = [Coords[i][iis] for i in range(Nparams)]
+        print(coord,iis)
+        xi0, xi2= compute_xiell_tables(coord,z=z,fid_dists=fid_dists, rmin=rmin, rmax=rmax, dr=dr)
+        
+        X0gridii[iis] = xi0
+        X2gridii[iis] = xi2
+        
+comm.Allreduce(X0gridii, X0grid, op=MPI.SUM)
+comm.Allreduce(X2gridii, X2grid, op=MPI.SUM)
+
+del(X0gridii, X2gridii)
+            
+# Now compute the derivatives
+
+if mpi_rank == 0:
+
+    derivs0 = compute_derivatives(X0grid, dxs, center_ii, 5)
+    derivs2 = compute_derivatives(X2grid, dxs, center_ii, 5)
+
+    # Now save:
+    outfile = basedir + 'emu/boss_z_%.2f_xiells.json'%(z)
+
+    list0 = [ dd.tolist() for dd in derivs0 ]
+    list2 = [ dd.tolist() for dd in derivs2 ]
+
+    outdict = {'params': ['omegam', 'h', 'sigma8'],\
+           'x0': x0s,\
+           'rvec': rr.tolist(),\
+           'derivs0': list0,\
+           'derivs2': list2,}
+
+    json_file = open(outfile, 'w')
+    json.dump(outdict, json_file)
+    json_file.close()
