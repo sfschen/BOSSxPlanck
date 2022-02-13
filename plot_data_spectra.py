51d32261620789e117346323e3832faef85c58e6
Martin J White <mwhite@berkeley.edu>
2022-02-12 19:01:17 -0800

    Add xi_ell 1st pass.

--- plot_data_spectra.py
+++ plot_data_spectra.py
@@ -19,9 +19,9 @@ llst = ['--',':' ]
 def make_pkl_plot():
     """Does the work of making the P_ell(k) figure."""
     rsddb  = "rsd_data/"
-    fig,ax = plt.subplots(2,2,figsize=(8,3.5),sharex=True,\
+    fig,ax = plt.subplots(2,2,figsize=(8,3.1),sharex=True,\
                gridspec_kw={'height_ratios':[3,1]})
-    # The multipole power spectra.
+    # The multipole power spectra -- NEED TO FIX ERROR BARS.
     for i,zz in enumerate(zlst):
         for j,hemi in enumerate(['NGC','SGC']):
             offs=0.001 if j==0 else -0.001
@@ -42,7 +42,7 @@ def make_pkl_plot():
             ax[j,i].set_xlim(0.005,0.25)
         ax[0,i].set_ylim(-100,2250)
         ax[1,i].set_ylim(0,2)
-        ax[1,i].set_xlabel(r'$k\quad [h\ {\rm Mpc}]$')
+        ax[1,i].set_xlabel(r'$k\quad [h\ {\rm Mpc}^{-1}]$')
     ax[0,0].set_ylabel(r'$k P_\ell(k)\quad [h^2{\rm Mpc}^{-2}]$')
     ax[1,0].set_ylabel(r'Ratio')
     for j in range(2):
@@ -58,8 +58,35 @@ def make_pkl_plot():
 
 def make_xil_plot():
     """Does the work of making the xi_ell(s) figure."""
-    # For now a stub.
-    pass
+    rsddb  = "rsd_data/"
+    fig,ax = plt.subplots(2,2,figsize=(8,3.1),sharex=True,\
+               gridspec_kw={'height_ratios':[3,1]})
+    # The multipole power spectra.
+    for i,zz in enumerate(zlst):
+        iz = 1 if i==0 else 3
+        # The data itself -- NEED TO FIX ERROR BARS.
+        xi = np.loadtxt(rsddb+"xi/z{:d}.xi".format(iz))
+        ax[0,i].errorbar(xi[:,0],xi[:,0]**2*xi[:,1],yerr=10,\
+                         color=clst[0],fmt='s',mfc='None',label=r'$\ell=0$')
+        ax[0,i].errorbar(xi[:,0],xi[:,0]**2*xi[:,2],yerr=10,\
+                         color=clst[0],fmt='^',mfc='None',label=r'$\ell=2$')
+        ax[0,i].legend(title="$z={:.2f}$".format(zz),loc=1)
+        # Now the ratio to the theory.
+        ax[1,i].axhline(1.0,ls=':',color='k')
+        #
+        for j in range(2):
+            ax[j,i].set_xlim(75,125)
+        ax[0,i].set_ylim(0,100)
+        ax[1,i].set_ylim(0,2)
+        ax[1,i].set_xlabel(r'$s\quad [h^{-1}{\rm Mpc}]$')
+    ax[0,0].set_ylabel(r'$i^\ell s^2 \xi_\ell(s)\quad [h^{-2}{\rm Mpc}^{2}]$')
+    ax[1,0].set_ylabel(r'Ratio')
+    for j in range(2):
+        ax[j,1].get_yaxis().set_visible(False)
+    #
+    plt.tight_layout()
+    plt.savefig('plot_data_xil.pdf')
+    #
 
 
 
@@ -67,7 +94,7 @@ def make_xil_plot():
 def make_cls_plot():
     """Does the work of making the C_ell figure."""
     angdb  = "data/"
-    fig,ax = plt.subplots(2,2,figsize=(8,3.5),sharex=True,\
+    fig,ax = plt.subplots(2,2,figsize=(8,3.1),sharex=True,\
                gridspec_kw={'height_ratios':[3,1]})
     # The angular power spectra.
     for i,zz in enumerate(zlst):
