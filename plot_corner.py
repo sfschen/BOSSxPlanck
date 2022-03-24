1e52cafee104de82dce418c9f52ae8b697acae98
Martin J White <mwhite@berkeley.edu>
2022-03-24 14:30:37 -0700

    Adding Planck.

--- plot_corner.py
+++ plot_corner.py
@@ -21,9 +21,10 @@ db   = "/global/cscratch1/sd/mwhite/BOSS/Cobaya/"
 chains = []
 clist  = []
 llist  = []
-legnd  = [r'RSD+BAO',r'+$\kappa g$',r'+$\kappa g$ (SZ)',r'+$\kappa g\ (\ell>80)$']
+legnd  = [r'Planck18',r'RSD+BAO',r'+$\kappa g$',r'+$\kappa g$ (SZ)',r'+$\kappa g\ (\ell>80)$']
 icol   = 0
 for yaml in [\
+            "planck18.yaml",\
             "rsd_bao_zall.yaml",\
             "rxk_ngc_lcdm.yaml",\
             "rxk_nosz_ngc_lcdm.yaml",\
@@ -32,15 +33,21 @@ for yaml in [\
     info= yaml_load_file(db + yaml)
     cc  = loadMCSamples(db + info["output"],no_cache=True,\
                         settings={'ignore_rows':0.3})
+    cc.weighted_thin(2)
     p   = cc.getParams()
+    if yaml.find('planck18')>=0: # Do some renaming.
+        p.omegam = p.Omega_m
     cc.addDerived(p.sigma8*(p.omegam/0.3)**0.5,name='S8',label='S_8')
     chains.append(cc)
     #
-    col = 'C'+str(icol)
+    if yaml.find('planck18')>=0: # Use grey for Planck.
+        col = 'darkgrey'
+    else:
+        col = 'C'+str(icol)
+        icol += 1
     clist.append(col)
     llist.append({'ls':'-','color':col})
     legnd.append(yaml[:-5])
-    icol += 1
 #
 for cc in chains:
     print('\n'+cc.getName())
@@ -59,7 +66,7 @@ g.triangle_plot(chains,\
                 ["omegam","H0","sigma8"],\
                 colors=clist,line_args=llist,\
                 legend_labels=legnd,\
-                filled=[True,True,False,False],\
-                alphas=[0.40,0.40,1.000,1.000])
+                filled=[True,True,True,False,False],\
+                alphas=[1.00,0.40,0.40,1.000,1.000])
 g.export('corner_plot.pdf')
 #
